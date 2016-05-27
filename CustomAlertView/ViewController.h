efd804728a36dadee491c096e86372aa8f241ae5
codeliu6572 <codeliu6572@163.com>
2016-05-27 09:43:24 +0800

    Initial Commit

---
+++ CustomAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertView
+//
+//  Created by 刘浩浩 on 16/5/27.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomAlertView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomAlertView
+//
+//  Created by 刘浩浩 on 16/5/27.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
